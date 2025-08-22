d28c63ee5ebd0b4a53d23f4fd57811b8a4bdfa28
razornez <razornez@gmail.com>
2025-08-22 15:13:52 +0700

    enhance

--- public/index.php
+++ public/index.php
@@ -1,20 +1,25 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
+// Maintenance check
+if (file_exists(__DIR__.'/storage/framework/maintenance.php')) {
+    require __DIR__.'/storage/framework/maintenance.php';
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
-
-// Bootstrap Laravel and handle the request...
-/** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// Jalankan request
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = tap($kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+))->send();
+
+$kernel->terminate($request, $response);
